b8dfab5d48fe9541d79991a247ce14f8ceb18e73
Crowdbotics <team@crowdbotics.com>
2024-03-01 03:09:50 +0000

    Initial commit

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "little-dawn-46739",
+  url: "https://little-dawn-46739.botics.co",
+  api: "https://little-dawn-46739.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
